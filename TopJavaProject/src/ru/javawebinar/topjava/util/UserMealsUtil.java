7d1f8e8dc5261da2a4d906f6368a7c54d172c583
Eugen1974 <eugen_ua@mail.ru>
2017-10-07 23:27:05 +0300

    3th commit (without lambda)

--- TopJavaProject/src/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ TopJavaProject/src/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -1,15 +1,18 @@
 package ru.javawebinar.topjava.util;
 
-import ru.javawebinar.topjava.model.UserMeal;
-import ru.javawebinar.topjava.model.UserMealWithExceed;
-
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.Month;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
+
+import ru.javawebinar.topjava.model.UserMeal;
+import ru.javawebinar.topjava.model.UserMealWithExceed;
 
 public class UserMealsUtil {
+
 	public static void main(String[] args) {
 		List<UserMeal> mealList = Arrays.asList(
 				new UserMeal(LocalDateTime.of(2015, Month.MAY, 30, 10, 0), "Завтрак", 500),
@@ -18,11 +21,23 @@ public class UserMealsUtil {
 				new UserMeal(LocalDateTime.of(2015, Month.MAY, 31, 10, 0), "Завтрак", 1000),
 				new UserMeal(LocalDateTime.of(2015, Month.MAY, 31, 13, 0), "Обед", 500),
 				new UserMeal(LocalDateTime.of(2015, Month.MAY, 31, 20, 0), "Ужин", 510));
-		getFilteredWithExceeded(mealList, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000);
+		getFilteredWithExceeded(mealList, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000).forEach(System.out::println);
 	}
 
 	public static List<UserMealWithExceed> getFilteredWithExceeded(List<UserMeal> mealList, LocalTime startTime,
 			LocalTime endTime, int caloriesPerDay) {
-		return null;
+		List<UserMealWithExceed> result = new ArrayList<>();
+		for (UserMeal meal1 : mealList) {
+			if (TimeUtil.isBetween(meal1.getDateTime().toLocalTime(), startTime, endTime)) {
+				int sumCalories = 0;
+				for (UserMeal meal2 : mealList) {
+					if (Objects.equals(meal1.getDateTime().toLocalDate(), meal2.getDateTime().toLocalDate())) {
+						sumCalories += meal2.getCalories();
+					}
+				}
+				result.add(new UserMealWithExceed(meal1, sumCalories > caloriesPerDay));
+			}
+		}
+		return result;
 	}
 }
